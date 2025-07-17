8b140535d22576d33feb1fcd02066bf76f99fae9
Payal <payalgupta425@gmail.com>
2025-07-18 01:57:22 +0530

    deploy ready

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/PortFolio/',
   plugins: [react(),tailwindcss(),],
   base: '/PortFolio/',
 })
